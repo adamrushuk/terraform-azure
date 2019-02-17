c04017e9a60653c1e016c473761778054320e663
Adam Rush <adamrushuk@gmail.com>
2019-02-17 16:06:14 +0000

    Added Azure Remote Backend example Terraform configuration

---
+++ examples/remote-backend/terraform.tfvars
@@ -0,0 +1,5 @@
+# Assign variables
+location = "eastus"
+resource_group_name = "backend-test-rg"
+virtual_network_name = "test-vnet"
+address_space = ["10.0.0.0/16"]
---
+++ examples/remote-backend/variables.tf
@@ -0,0 +1,9 @@
+# Declare variables
+
+variable "location" {}
+variable "resource_group_name" {}
+variable "virtual_network_name" {}
+
+variable "address_space" {
+  type = "list"
+}
