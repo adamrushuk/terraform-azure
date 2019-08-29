09df1e7fbea1efa75c95e13c65360abaa199be84
Adam Rush <adamrushuk@gmail.com>
2019-08-29 07:53:35 +0100

    Fixed cleanup logic to handle multiple tags

--- scripts/Delete-ResourceGroupNotTaggedKeep.ps1
+++ scripts/Delete-ResourceGroupNotTaggedKeep.ps1
@@ -1,4 +1,4 @@
-# Delete EVERY resource group that does NOT have tag: keep=true
-$jobs = Get-AzResourceGroup | Where-Object {$_.Tags -eq $null -or $_.Tags.GetEnumerator().Name -ne "keep"} | Remove-AzResourceGroup -Force -AsJob
+# Delete EVERY resource group that does NOT include tag: keep=true
+$jobs = Get-AzResourceGroup | Where-Object {$_.Tags -eq $null -or $_.Tags.GetEnumerator().Name -notcontains "keep"} | Remove-AzResourceGroup -Force -AsJob
 $jobs | Wait-Job
 $jobs | Receive-Job -Keep
