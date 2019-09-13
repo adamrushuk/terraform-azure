d6edb7e7f8d1a2aff1fa2596f8cf795eaef29068
Adam Rush <adamrushuk@gmail.com>
2019-09-13 11:17:53 +0100

    Added testing script

---
+++ temp/testing.ps1
@@ -0,0 +1,17 @@
+# Ansible TF build testing
+cd ~\Code\terraform-azure\examples\linux-vm-remoteexec-ansible
+
+terraform init
+terraform plan
+
+~\Code\terraform-azure\scripts\Start-Terraform.ps1
+
+terraform taint null_resource.init
+terraform taint azurerm_virtual_machine.myterraformvm
+
+rm -Path "~/.ssh/known_hosts" -Force
+ssh azureuser@<VMNAME>.eastus.cloudapp.azure.com
+
+source ~/python-env/ansible2.8.4/bin/activate
+
+~\Code\terraform-azure\scripts\Delete-ResourceGroupNotTaggedKeep.ps1
