f58c5b2b4161ed80152025bb1cf2106693042621
Adam Rush <adamrushuk@gmail.com>
2019-10-08 08:04:43 +0100

    Added remote backend

--- examples/hub-spoke-network/05-main.tf
+++ examples/hub-spoke-network/05-main.tf
@@ -1,3 +1,16 @@
+# Create a Remote Backend
+terraform {
+  backend "azurerm" {
+    # storage_account_name needs to be changed manually as variables are not supported.
+    # Should be a format like "<random chars>terraform", eg: "am1ojbxsfterraform"
+    storage_account_name = "<REPLACE-WITH-YOUR-STORAGE-ACCOUNT-NAME>"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+
+    # access_key provided via ARM_ACCESS_KEY env var
+  }
+}
+
 provider "azurerm" {
   # Pin version as per best practice
   version = "=1.35.0"
