faaee75615facac7c1d07b6c6d1a24bf970d71ad
Adam Rush <rusha103@mod.gov.uk>
2019-11-27 13:25:30 +0000

    Changed deprecated v0.12 interpolation syntax

--- examples/aks-monitoring/main.tf
+++ examples/aks-monitoring/main.tf
@@ -4,22 +4,22 @@
 
 resource "azurerm_resource_group" "example" {
   name     = "${var.prefix}-mon-resources-rg"
-  location = "${var.location}"
+  location = var.location
 }
 
 resource "azurerm_log_analytics_workspace" "example" {
   name                = "${var.prefix}-law"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
 }
 
 resource "azurerm_log_analytics_solution" "example" {
   solution_name         = "Containers"
-  location              = "${azurerm_resource_group.example.location}"
-  resource_group_name   = "${azurerm_resource_group.example.name}"
-  workspace_resource_id = "${azurerm_log_analytics_workspace.example.id}"
-  workspace_name        = "${azurerm_log_analytics_workspace.example.name}"
+  location              = azurerm_resource_group.example.location
+  resource_group_name   = azurerm_resource_group.example.name
+  workspace_resource_id = azurerm_log_analytics_workspace.example.id
+  workspace_name        = azurerm_log_analytics_workspace.example.name
 
   plan {
     publisher = "Microsoft"
@@ -29,8 +29,8 @@ resource "azurerm_log_analytics_solution" "example" {
 
 resource "azurerm_kubernetes_cluster" "example" {
   name                = "${var.prefix}-mon"
-  location            = "${azurerm_resource_group.example.location}"
-  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   dns_prefix          = "${var.prefix}-mon"
 
   agent_pool_profile {
@@ -42,14 +42,14 @@ resource "azurerm_kubernetes_cluster" "example" {
   }
 
   service_principal {
-    client_id     = "${var.kubernetes_client_id}"
-    client_secret = "${var.kubernetes_client_secret}"
+    client_id     = var.kubernetes_client_id
+    client_secret = var.kubernetes_client_secret
   }
 
   addon_profile {
     oms_agent {
       enabled                    = true
-      log_analytics_workspace_id = "${azurerm_log_analytics_workspace.example.id}"
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
     }
   }
 
