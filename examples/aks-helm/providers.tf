943736b342fbb2d579859db383113dd1892da354
Adam Rush <rusha103@mod.gov.uk>
2020-02-07 19:46:14 +0000

    Added helm release (not working atm)

--- examples/aks-helm/providers.tf
+++ examples/aks-helm/providers.tf
@@ -8,14 +8,16 @@ provider "azurerm" {
   version = "=1.43.0"
 }
 
-provider "kubernetes" {
+# provider "kubernetes" {
 
-  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
-}
+#   host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+#   client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+#   client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+#   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+# }
 
+# Check latest release notes and CHANGELOG
+# https://github.com/terraform-providers/terraform-provider-helm/blob/master/CHANGELOG.md
 provider "helm" {
   kubernetes {
     host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
